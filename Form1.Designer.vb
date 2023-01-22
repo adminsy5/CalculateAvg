d88043ee344a71a9e069400e29ee50eeb9847b0c
adminsy5 <piyush@cbigujarat.onmicrosoft.com>
2023-01-22 09:19:24 +0530

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,220 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.android = New System.Windows.Forms.Label()
+        Me.sub1t = New System.Windows.Forms.TextBox()
+        Me.sub2t = New System.Windows.Forms.TextBox()
+        Me.Isx = New System.Windows.Forms.Label()
+        Me.sub4t = New System.Windows.Forms.TextBox()
+        Me.dotnet = New System.Windows.Forms.Label()
+        Me.sub3t = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.sub5t = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.totalt = New System.Windows.Forms.Label()
+        Me.avgt = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Calculate = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 13.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(184, 33)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(290, 47)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Calculate the Avg"
+        '
+        'android
+        '
+        Me.android.AutoSize = True
+        Me.android.Location = New System.Drawing.Point(148, 183)
+        Me.android.Name = "android"
+        Me.android.Size = New System.Drawing.Size(122, 32)
+        Me.android.TabIndex = 1
+        Me.android.Text = "Android-2"
+        '
+        'sub1t
+        '
+        Me.sub1t.Location = New System.Drawing.Point(395, 180)
+        Me.sub1t.Name = "sub1t"
+        Me.sub1t.Size = New System.Drawing.Size(132, 39)
+        Me.sub1t.TabIndex = 2
+        '
+        'sub2t
+        '
+        Me.sub2t.Location = New System.Drawing.Point(395, 239)
+        Me.sub2t.Name = "sub2t"
+        Me.sub2t.Size = New System.Drawing.Size(132, 39)
+        Me.sub2t.TabIndex = 10
+        '
+        'Isx
+        '
+        Me.Isx.AutoSize = True
+        Me.Isx.Location = New System.Drawing.Point(148, 242)
+        Me.Isx.Name = "Isx"
+        Me.Isx.Size = New System.Drawing.Size(137, 32)
+        Me.Isx.TabIndex = 9
+        Me.Isx.Text = "Info Sys (IS)"
+        '
+        'sub4t
+        '
+        Me.sub4t.Location = New System.Drawing.Point(395, 359)
+        Me.sub4t.Name = "sub4t"
+        Me.sub4t.Size = New System.Drawing.Size(132, 39)
+        Me.sub4t.TabIndex = 14
+        '
+        'dotnet
+        '
+        Me.dotnet.AutoSize = True
+        Me.dotnet.Location = New System.Drawing.Point(148, 362)
+        Me.dotnet.Name = "dotnet"
+        Me.dotnet.Size = New System.Drawing.Size(210, 32)
+        Me.dotnet.TabIndex = 13
+        Me.dotnet.Text = ".Net Programming"
+        '
+        'sub3t
+        '
+        Me.sub3t.Location = New System.Drawing.Point(395, 300)
+        Me.sub3t.Name = "sub3t"
+        Me.sub3t.Size = New System.Drawing.Size(132, 39)
+        Me.sub3t.TabIndex = 12
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(148, 303)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(206, 32)
+        Me.Label3.TabIndex = 11
+        Me.Label3.Text = "Int of Things (IOT)"
+        '
+        'sub5t
+        '
+        Me.sub5t.Location = New System.Drawing.Point(395, 424)
+        Me.sub5t.Name = "sub5t"
+        Me.sub5t.Size = New System.Drawing.Size(132, 39)
+        Me.sub5t.TabIndex = 16
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(148, 427)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(211, 32)
+        Me.Label4.TabIndex = 15
+        Me.Label4.Text = "Java Programming"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(215, 581)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(95, 32)
+        Me.Label2.TabIndex = 17
+        Me.Label2.Text = "Total = "
+        '
+        'totalt
+        '
+        Me.totalt.AutoSize = True
+        Me.totalt.Location = New System.Drawing.Point(316, 581)
+        Me.totalt.Name = "totalt"
+        Me.totalt.Size = New System.Drawing.Size(0, 32)
+        Me.totalt.TabIndex = 18
+        '
+        'avgt
+        '
+        Me.avgt.AutoSize = True
+        Me.avgt.Location = New System.Drawing.Point(312, 635)
+        Me.avgt.Name = "avgt"
+        Me.avgt.Size = New System.Drawing.Size(0, 32)
+        Me.avgt.TabIndex = 20
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(184, 635)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(123, 32)
+        Me.Label7.TabIndex = 19
+        Me.Label7.Text = "Avarage ="
+        '
+        'Calculate
+        '
+        Me.Calculate.Location = New System.Drawing.Point(260, 498)
+        Me.Calculate.Name = "Calculate"
+        Me.Calculate.Size = New System.Drawing.Size(150, 46)
+        Me.Calculate.TabIndex = 21
+        Me.Calculate.Text = "Calculate"
+        Me.Calculate.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(13.0!, 32.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(743, 1079)
+        Me.Controls.Add(Me.Calculate)
+        Me.Controls.Add(Me.avgt)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.totalt)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.sub5t)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.sub4t)
+        Me.Controls.Add(Me.dotnet)
+        Me.Controls.Add(Me.sub3t)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.sub2t)
+        Me.Controls.Add(Me.Isx)
+        Me.Controls.Add(Me.sub1t)
+        Me.Controls.Add(Me.android)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents android As Label
+    Friend WithEvents sub1t As TextBox
+    Friend WithEvents sub2t As TextBox
+    Friend WithEvents Isx As Label
+    Friend WithEvents sub4t As TextBox
+    Friend WithEvents dotnet As Label
+    Friend WithEvents sub3t As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents sub5t As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents totalt As Label
+    Friend WithEvents avgt As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Calculate As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Dim s1, s2, s3, s4, s5 As Integer
+    Private Sub Calculate_Click(sender As Object, e As EventArgs) Handles Calculate.Click
+        s1 = Val(sub1t.Text)
+        s2 = Val(sub2t.Text)
+        s3 = Val(sub3t.Text)
+        s4 = Val(sub4t.Text)
+        s5 = Val(sub5t.Text)
+
+        totalt.Text = (s1 + s2 + s3 + s4 + s5)
+        avgt.Text = totalt.Text / 5
+
+    End Sub
+End Class
